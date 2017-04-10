2f67159cb3149f1bee182fe3109abe867f41f07d
marcin <marcin@157.158.169.53>
2017-04-10 23:24:03 +0200

    druga klasa

---
+++ cistan/src/cistan/class2.java
@@ -0,0 +1,13 @@
+package cistan;
+
+public class class2 {
+	int a,b,c;
+
+	public class2(){
+		a=5;b=3;c=9;
+	}
+	public void tellme()
+	{
+		System.out.println(a+" "+ b + " " + c);
+	}
+}
--- cistan/src/cistan/mainclass.java
+++ cistan/src/cistan/mainclass.java
@@ -5,6 +5,8 @@ public class mainclass {
 	public static void main(String[] args) {
 			class1 klasa = new class1();
 			klasa.tellme();
+			class2 klasa2 = new class2();
+			klasa2.tellme();
 	}
 
 }
